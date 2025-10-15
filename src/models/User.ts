a41fb5a8d1c28cc5fb9402bc084e1eebd7e4d542
Titan6 <dotcubetitan6@gmail.com>
2025-10-15 12:05:58 +0530

    code refactored

--- src/models/User.ts
+++ src/models/User.ts
@@ -35,7 +35,7 @@ userSchema.pre('save',async function(next){
 })
 
 userSchema.methods.comparePassword = async function (candidatePassword:string){
-  return await bcrypt.compare(candidatePassword,this.Password)
+  return await bcrypt.compare(candidatePassword,this.password)
 }
 
 const User = model<IUser>("User", userSchema, "User");
